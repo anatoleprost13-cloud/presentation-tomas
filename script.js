2f047b80068a3e1f0de7a043d0a2af813f73a317
anatoleprost13-cloud <anatoleprost13@gmail.com>
2026-06-08 18:01:08 +0200

    premier commit propre

---
+++ script.js
@@ -0,0 +1,199 @@
+const media = [
+  { type: "image", src: "images/Sans titre-73.jpg" },
+  { type: "image", src: "images/Sans titre-74.jpg" },
+  { type: "image", src: "images/Sans titre-75.jpg" },
+  { type: "image", src: "images/IMG_3216.jpg" },
+  { type: "image", src: "images/IMG_3219.jpg" },
+  { type: "image", src: "images/insta journal.jpg" },
+  { type: "image", src: "images/4.jpg" },
+  { type: "image", src: "images/1.jpg" },
+  { type: "image", src: "images/2.jpg" },
+  { type: "image", src: "images/3.jpg" },
+  { type: "image", src: "images/5.jpg" },
+	
+	  { type: "image", src: "images/IMG_2604.jpg" },
+	{ type: "image", src: "images/IMG_2541.jpg" },
+	{ type: "image", src: "images/IMG_2542.jpg" },
+	{ type: "image", src: "images/IMG_2565.jpg" },
+	{ type: "image", src: "images/IMG_2605.jpg" },
+	{ type: "image", src: "images/IMG_2607.jpg" },
+	
+		  { type: "image", src: "images/bovins.jpg" },
+	{ type: "image", src: "images/bovins2.jpg" },
+	{ type: "image", src: "images/bovins3.jpg" },
+	{ type: "image", src: "images/affiche1RVBcouleur réelles2.jpg" },
+	{ type: "image", src: "images/affiche2RVBcouleur réelles.jpg" },
+	{ type: "image", src: "images/affiche3.jpg" },
+	
+			  { type: "image", src: "images/stickers copie.png" },
+	{ type: "image", src: "images/vvvvvvvv.jpg" },
+	{ type: "image", src: "images/Oraux2025-16.jpg" },
+	{ type: "image", src: "images/IMG_2935.jpg" },
+	{ type: "image", src: "images/IMG_2917.jpg" },
+	{ type: "image", src: "images/affiche scan copie.jpg" },
+	
+			  { type: "image", src: "images/villager 3.jpeg" },
+	{ type: "image", src: "images/villager 13.jpeg" },
+	{ type: "image", src: "images/36589174.jpeg" },
+	{ type: "image", src: "images/61A78oCHAoL._AC_UF1000,1000_QL80_.jpg" },
+	{ type: "image", src: "images/81Hjf3HHBHL.jpg" },
+	{ type: "image", src: "images/1109548039_L.jpg" },
+
+				  { type: "image", src: "images/9782364687516_1.jpg" },
+	{ type: "image", src: "images/ane1 005.jpg" },
+	{ type: "image", src: "images/images-original.png" },
+	{ type: "image", src: "images/montée de la vache vieux.jpg" },
+	{ type: "image", src: "images/s-l1200.jpg" },
+	{ type: "image", src: "images/unnamed-3.jpg" },
+	
+		  { type: "image", src: "images/scan 1.jpeg" },
+	{ type: "image", src: "images/scan 2.jpeg" },
+	{ type: "image", src: "images/scan 3.jpeg" },
+	{ type: "image", src: "images/scan.jpeg" },
+	
+	{ type: "image", src: "images/stickers copie.png" },
+	{ type: "image", src: "images/transparent.jpg" },
+	{ type: "image", src: "images/unnamed-3.jpg" },
+	{ type: "image", src: "images/villager 3.jpeg" },
+	{ type: "image", src: "images/villager 13.jpeg" },
+	{ type: "image", src: "images/vvvvvvvv.jpg" },
+	
+		{ type: "image", src: "images/personnages.jpg" },
+	{ type: "image", src: "images/IMG_4370.png" },
+	
+		{ type: "image", src: "images/colporteur.jpg" },
+	{ type: "image", src: "images/colporteur-1200x-q80.jpg" },
+	{ type: "image", src: "images/clean drawing.jpg" },
+	{ type: "image", src: "images/claqueeetes vertes.jpg" },
+	{ type: "image", src: "images/bleu danse.jpg" },
+	
+		{ type: "image", src: "images/légendes 1.jpeg" },
+	{ type: "image", src: "images/légendes 2.jpeg" },
+	{ type: "image", src: "images/légendes 3.jpeg" },
+	{ type: "image", src: "images/légendes 4.jpeg" },
+	{ type: "image", src: "images/légendes 5.jpeg" },
+		{ type: "image", src: "images/légendes 6.jpeg" },
+	{ type: "image", src: "images/légendes 7.jpeg" },
+	{ type: "image", src: "images/légendes 8.jpeg" },
+	{ type: "image", src: "images/légendes 9.jpeg" },
+	{ type: "image", src: "images/légendes 10.jpeg" },
+		{ type: "image", src: "images/légendes 11.jpeg" },
+	{ type: "image", src: "images/légendes 12.jpeg" },
+	{ type: "image", src: "images/légendes 13.jpeg" },
+	{ type: "image", src: "images/légendes 14.jpeg" },
+	{ type: "image", src: "images/légendes 15.jpeg" },
+		{ type: "image", src: "images/légendes 16.jpeg" },
+	{ type: "image", src: "images/légendes 17.jpeg" },
+	{ type: "image", src: "images/légendes 18.jpeg" },
+	{ type: "image", src: "images/légendes 19.jpeg" },
+	{ type: "image", src: "images/légendes 20.jpeg" },
+		{ type: "image", src: "images/légendes 21.jpeg" },
+		{ type: "image", src: "images/légendes 22.jpeg" },
+	{ type: "image", src: "images/légendes 23.jpeg" },
+	{ type: "image", src: "images/légendes 24.jpeg" },
+	{ type: "image", src: "images/légendes 25.jpeg" },
+	{ type: "image", src: "images/légendes 26.jpeg" },
+		{ type: "image", src: "images/légendes 27.jpeg" },
+	{ type: "image", src: "images/légendes 28.jpeg" },
+	{ type: "image", src: "images/légendes.jpeg" },
+
+
+
+	
+  { type: "video", src: "videos/IMG_2477.MOV" },
+  { type: "video", src: "videos/Clip22.m4v" },
+	  { type: "video", src: "videos/filtered-A239D633-1533-4CDA-AA88-FAF91235E5E1.mp4" },
+	  { type: "video", src: "videos/IMG_1286.MOV" },
+	  { type: "video", src: "videos/Clip23.m4v" },
+	{ type: "video", src: "videos/IMG_3330.MOV" },
+		{ type: "video", src: "videos/Bourrée à quatre  MORVAN ..mp4" },
+		{ type: "video", src: "videos/Les Galvachers du Morvan - Patinée et défilade.mp4" },
+	{ type: "video", src: "videos/15 Anost parade finale.mp4" },
+];
+
+const stage = document.getElementById("stage");
+
+let zIndex = 1;
+let deck = [];
+
+/* mélange Fisher-Yates */
+function shuffle(array) {
+  let a = [...array];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+/* reset du cycle */
+function resetDeck() {
+  deck = shuffle(media);
+}
+
+/* création bloc */
+function addBlock(x, y) {
+
+  // si vide → reshuffle
+  if (deck.length === 0) {
+    resetDeck();
+  }
+
+  const item = deck.pop(); // IMPORTANT : jamais de répétition dans un cycle
+
+  const block = document.createElement("div");
+  block.classList.add("block");
+
+  // couleurs fluo
+  block.classList.add(Math.random() > 0.5 ? "yellow" : "pink");
+
+  const size = 240;
+  block.style.width = size + "px";
+  block.style.height = size + "px";
+
+  block.style.left = (x - size / 2) + "px";
+  block.style.top = (y - size / 2) + "px";
+
+  block.style.zIndex = zIndex++;
+
+  let el;
+
+  if (item.type === "image") {
+    el = new Image();
+    el.src = item.src;
+  }
+
+  if (item.type === "video") {
+    el = document.createElement("video");
+    el.src = item.src;
+    el.muted = true;
+    el.autoplay = true;
+    el.loop = true;
+    el.playsInline = true;
+  }
+
+  block.appendChild(el);
+  stage.appendChild(block);
+}
+
+/* initialisation */
+resetDeck();
+
+/* clic = apparition séquentielle aléatoire */
+document.addEventListener("click", (e) => {
+  addBlock(e.clientX, e.clientY);
+});
+
+document.addEventListener("click", function () {
+  const music = document.getElementById("bgMusic");
+  music.play();
+}, { once: true });
+
+function toggleMusic() {
+  const music = document.getElementById("bgMusic");
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+}
